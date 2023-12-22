d56a8af58a65d3c274a1c047e18cba3602c359ad
Kalmuroth <dokkan.dracktar@gmail.com>
2023-12-22 13:40:49 +0100

    added gitignore + axios + lambda invoke/response

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var mongoClient = require("mongodb").MongoClient;
-
-mongoClient.connect("mongodb://db-student-m2-iot-france:DXQpNMiPQ6olwG68S4NwOyngh65MVktLNBYJmeI0gR0MDzE9HWrlDSRWHkf3aw3BVFMMqKfac4X3ACDbaahtpw%3D%3D@db-student-m2-iot-france.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@db-student-m2-iot-france@", function (err, client) {
-  client.close();
-});
+var axios = require('axios');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  try {
+    const response = await axios.get('https://swapi.dev/api/people/');
+    const data = response.data; // Store the response data
+    res.render('index', { title: 'Star Wars Characters', data: data }); // Render your index view with data
+  } catch (error) {
+    console.error('Error calling API:', error);
+    res.status(500).send('Error fetching data'); // Send an error response if there's an issue
+  }
 });
 
 module.exports = router;

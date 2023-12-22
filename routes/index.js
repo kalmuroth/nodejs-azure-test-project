4000c292c823c5624c83ba8cd4e9c360d0c2fbf2
Kalmuroth <dokkan.dracktar@gmail.com>
2023-12-22 14:32:26 +0100

    api call trigger (it work now)

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,18 @@ var axios = require('axios');
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   try {
-    const response = await axios.get('https://swapi.dev/api/people/');
-    const data = response.data; // Store the response data
-    res.render('index', { title: 'Star Wars Characters', data: data }); // Render your index view with data
+    const azureFunctionUrl = 'https://function-trigger-run-master.azurewebsites.net/api/HttpTrigger/'; 
+    const response = await axios.get(azureFunctionUrl, {
+      params: {
+        locationCityCountry: 'people' 
+      }
+    });
+
+    const data = response.data;
+    
+    res.render('index', { title: 'Star Wars Characters', data: data });
   } catch (error) {
-    console.error('Error calling API:', error);
-    res.status(500).send('Error fetching data'); // Send an error response if there's an issue
+    res.status(500).send('Error calling Azure Function');
   }
 });
 
